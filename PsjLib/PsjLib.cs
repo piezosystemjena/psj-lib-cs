7195ca7ad0fb255ad69f766c31ba66547fa1529a
Sidney Krombholz PSJ <skrombholz@piezojena.com>
2026-03-06 11:42:46 +0100

    Keep version numbers aligned

--- PsjLib/PsjLib.cs
+++ PsjLib/PsjLib.cs
@@ -1,4 +1,5 @@
 global using System.Collections.Generic;
+global using System.Reflection;
 
 namespace PsjLib;
 
@@ -9,6 +10,12 @@ public static class LibraryVersion
 {
     /// <summary>
     /// Semantic version of this library build.
+    /// Value is sourced from assembly metadata generated from the project file.
     /// </summary>
-    public const string Version = "1.0.0";
+    public static string Version =>
+        typeof(LibraryVersion).Assembly
+            .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+            ?.InformationalVersion
+        ?? typeof(LibraryVersion).Assembly.GetName().Version?.ToString()
+        ?? "0.0.0";
 }
